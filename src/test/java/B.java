e4b32c5a9951403b26d4a44578f02fa838074b34
bkaveri <bhaskarkaveri2702@gmail.com>
2020-06-02 18:18:26 +0530

    code added

---
+++ src/test/java/B.java
@@ -0,0 +1,16 @@
+
+
+import org.junit.Test;
+
+public class B {
+	@Test
+	public void test9() {
+		System.out.println("oracle");
+	}
+	
+	@Test
+	public void test8() {
+		System.out.println("sql");
+	}
+
+}
